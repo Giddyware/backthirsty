2ae058508e79752f120b90fa09fde85351100b11
Giddyware <idowugidiyanuoluwa@gmail.com>
2024-11-11 11:48:48 +0100

    feat:  add footer to page

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import { CalculateEarnings } from "@/components/calculate-earnings";
 import { DiveIntoTheFuture } from "@/components/dive-into-the-future";
 import { FaqSection } from "@/components/faq-section";
 import { FeaturesSection } from "@/components/features-section";
+import { Footer } from "@/components/footer";
 import { GetStarted } from "@/components/get-started";
 import { HeroSection } from "@/components/hero-section";
 import { Newsletter } from "@/components/newsletter";
 import { Testimonial } from "@/components/testimonial";
+import { navigationConstants } from "@/constants/navigation-link";
 
 export default function Home() {
   return (
@@ -18,6 +20,7 @@ export default function Home() {
       <FaqSection />
       <CalculateEarnings />
       <Newsletter />
+      <Footer navigation={navigationConstants} />
     </div>
   );
 }
