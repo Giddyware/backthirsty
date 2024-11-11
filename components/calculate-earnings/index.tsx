79d077aeceb2367d2eca578c71a0f2cb00ff0204
Giddyware <idowugidiyanuoluwa@gmail.com>
2024-11-11 12:20:26 +0100

    chore: fix alignment in sections

--- components/calculate-earnings/index.tsx
+++ components/calculate-earnings/index.tsx
@@ -3,14 +3,16 @@ import Image from "next/image";
 export const CalculateEarnings = () => {
   return (
     <section className="mx-auto px-6 py-20 flex sm:py-28 gap-24 lg:px-20 lg:py-20">
-      <Image
-        src="/calculator.png"
-        className=""
-        alt="calculator"
-        height={420}
-        width={490}
-      />
-      <div className="space-y-7">
+      <div className="flex-1">
+        <Image
+          src="/calculator.png"
+          className=""
+          alt="calculator"
+          height={420}
+          width={490}
+        />
+      </div>
+      <div className="space-y-7 flex-1">
         <h3 className="font-bold text-left text-5xl max-w-xl mx-auto mb-12">
           Calculate Now and Discover Your Potential Earnings
         </h3>
