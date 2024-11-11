f37fd65934e0d6b401825c82b4e34c935e30cc7b
Giddyware <idowugidiyanuoluwa@gmail.com>
2024-11-11 11:07:07 +0100

    chore: add calculate earnings section

---
+++ components/calculate-earnings/index.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export const CalculateEarnings = () => {
+  return (
+    <section className="mx-auto px-6 py-20 flex sm:py-28 gap-24 lg:px-20 lg:py-20">
+      <Image
+        src="/calculator.png"
+        className=""
+        alt="calculator"
+        height={420}
+        width={490}
+      />
+      <div className="space-y-7">
+        <h3 className="font-bold text-left text-5xl max-w-xl mx-auto mb-12">
+          Calculate Now and Discover Your Potential Earnings
+        </h3>
+
+        <p className="text-[#7A7A7A] max-w-[540px] text-[24px] leading-[29.04px]">
+          Use our investment calculator to see how much you could have earned
+          from past investments in stocks and cryptocurrencies. Simply fill in
+          the details and uncover the potential growth of your portfolio.&quot;
+        </p>
+      </div>
+    </section>
+  );
+};
