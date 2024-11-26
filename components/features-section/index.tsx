d540a8a8c7b9a7255f332da9a677addbabe12091
Giddyware <idowugidiyanuoluwa@gmail.com>
2024-11-26 21:04:43 +0100

    chore: make features section responsive

--- components/features-section/index.tsx
+++ components/features-section/index.tsx
@@ -2,8 +2,8 @@ import { CardContainer } from "../feature-card";
 
 export const FeaturesSection = () => {
   return (
-    <section className="mx-auto px-6 py-20 sm:py-28 lg:items-center lg:gap-x-10 lg:px-8 lg:py-20">
-      <h3 className="text-center font-bold text-5xl max-w-2xl mx-auto mb-12">
+    <section className="mx-auto px-6 py-20 sm:py-28 lg:items-center lg:gap-x-10 lg:px-8 lg:py-20 space-y-10">
+      <h3 className="text-center font-bold text-3xl md:text-4xl lg:text-5xl max-w-2xl mx-auto mb-12">
         Why You Should Start Investing Now
       </h3>
       <CardContainer />
