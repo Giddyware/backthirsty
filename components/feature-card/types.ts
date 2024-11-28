1839541cd2f57e06e564c73bbbee3cda16c3eeec
Giddyware <idowugidiyanuoluwa@gmail.com>
2024-11-28 23:21:30 +0100

    chore: type feature card

---
+++ components/feature-card/types.ts
@@ -0,0 +1,12 @@
+export interface FeatureCardProps {
+  title: string;
+  description: string;
+  icon: React.ReactNode;
+  href?: string;
+}
+
+export interface Feature {
+  title: string;
+  description: string;
+  icon: React.ReactNode;
+}
